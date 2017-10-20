6bef86dc221e8e5bfacd11f2220bb73298b53775
1412414 <thienphuvuong@gmail.com>
2017-10-20 16:42:43 +0700

    init

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret'], scope: 'public_profile, email', info_fields: 'id,first_name,middle_name,last_name,email,name,link'
+provider :facebook, ENV['app_id'], ENV['secret'], scope: 'public_profile, email', info_fields: 'id,first_name,middle_name,last_name,email,name,link'
 end
